b81b31fec2ba699d05318868637579e34f2ac62f
shehrozkarim69 <153180186+shehrozkarim69@users.noreply.github.com>
2024-07-08 17:33:12 +0500

    sockets

--- app.js
+++ app.js
@@ -2,26 +2,25 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const app = express();
-var io = require('socket.io')(server);
 
 const server = http.createServer(app);
+const io = require('socket.io')(server);
 
+app.get('/', (req, res) => {
+    const options = {
+        root: path.join(__dirname)
+    };
+    const filename = 'index.html';
+    res.sendFile(filename, options);
+});
 
-app.get('/',(req, res)=>{
-
-const option ={
-    root: path.join(__dirname)
-   
-}
-     const filename = 'index.html';
-    res.sendFile(filename,option);
-})
-
-
-
-io.on("connection",(req,res) => {
+io.on("connection", (socket) => {
     console.log("User connected");
-})
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
 server.listen(3000, () => {
     console.log('Server is listening at port 3000');
